08bded0edaf284c4e482b228ab47133e3fbb65de
yilyan <yilyangr19@gmail.com>
2025-09-25 20:29:24 -0500

    Agregue unos cambios en los estilos con Bootstrap y css

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ $query = mysqli_query($con, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuarios CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="CSS/style.css">
+    
 </head>
 
 <body>
